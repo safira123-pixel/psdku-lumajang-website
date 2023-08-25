eae83bbdd0bd1d416a5837f16afb82f60c99bf02
safira123-pixel <safiraistifarini@gmail.com>
2023-08-25 10:45:01 +0700

    updated

--- client/src/AppRoute.js
+++ client/src/AppRoute.js
@@ -1,5 +1,5 @@
 import { Route, Switch } from 'react-router-dom'
-import { HomePage, ProfilePage, selayang_pandang } from './apps/pages'
+import { HomePage, ProfilePage, selayang_pandang, akutansi } from './apps/pages'
 
 const AppRoute = () => {
     return (
@@ -7,6 +7,7 @@ const AppRoute = () => {
             <Route exact path='/' component={HomePage} />
             <Route exact path='/about/profile' component={ProfilePage} />
             <Route exact path='/about/selayang_pandang' component={selayang_pandang} />
+            <Route exact path='/akuntansi' component={akutansi} />
         </Switch>
     )
 }
--- client/src/apps/pages/index.js
+++ client/src/apps/pages/index.js
@@ -1,9 +1,11 @@
 import HomePage from './Home'
 import ProfilePage from './Profile'
 import selayang_pandang from './selayang_pandang'
+import akutansi from './akutansi'
 
 export {
     HomePage,
     ProfilePage,
     selayang_pandang,
+    akutansi
 }
